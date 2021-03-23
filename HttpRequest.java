d0e4a9ad8c44d0ea2ffa7fac19802aa5575ddc8c
Arya Parvizi <aresparvizi@gmail.com>
2021-03-23 19:29:56 +0430

    added URL and created a meaningful structure for the HTTP connection

--- HttpRequest.java
+++ HttpRequest.java
@@ -3,6 +3,7 @@ package main;
 import java.io.*;
 import java.net.*;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class HttpRequest {
@@ -11,6 +12,8 @@ public class HttpRequest {
     private HashMap<String, String> headers;
     private HashMap<String, String> params;
     private HashMap<String, String> body;
+
+    private static final String CLRF = "\r\n";
 
     public HttpRequest(String url) {
         this(url, HttpRequestMethod.GET, new HashMap<>(), new HashMap<>(), new HashMap<>());
@@ -66,23 +69,51 @@ public class HttpRequest {
     }
 
     public void setBody(HashMap<String, String> body) {
-        if(!httpRequestMethod.equals(HttpRequestMethod.GET)) // GET has no body that's why i put this if here.
-            this.body = body;
-
+        this.body = body;
     }
 
     public HttpResponse request() throws HttpException, IOException {
         // TODO
+
+        // request line --> general form of : Method<sp>Path<sp>Version<CLRF>
+        httpRequestMethod.toString() + url.
+        clearBody();
+
+        queryParams();
         String jsonBody = toJSON();
+
+
+
     }
 
     private String toJSON(){
         StringBuilder requestBody = new StringBuilder("{");
-        for(String jsonElement:body.keySet()){
-            requestBody.append("\n"+jsonElement);
-            requestBody.append(":"+body.get(jsonElement));            
+        Iterator<String> iterator = body.keySet().iterator();
+        if(iterator.hasNext())
+            requestBody.append(CLRF + iterator.next());
+        while (iterator.hasNext()){
+            String jsonElement = iterator.next();
+            requestBody.append(requestBody.append("," + CLRF)
+                    .append(jsonElement));
+
+            requestBody.append(requestBody.append(":")
+                    .append(body.get(jsonElement)));
         }
-        requestBody.append("\n}");
+        requestBody.append(CLRF + "}");
         return requestBody.toString();
+    }
+
+    private void clearBody(){ // clear the body if it is a GET request method.
+        if(httpRequestMethod.equals(HttpRequestMethod.GET))
+            body = new HashMap<>();
+        // else nothing.
+    }
+
+    private void queryParams(){
+        // for the get method.
+
+        if(!httpRequestMethod.equals(HttpRequestMethod.GET))
+            params = new HashMap<>();
+
     }
 }
